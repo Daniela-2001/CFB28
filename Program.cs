c20c21e39bc1743f1a78dd88ca8435f2627dba93
Daniela Duarte <danielatavaresd@gmail.com>
2024-02-06 23:11:49 +0000

    CFB28

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace aula28
+{
+    public class Jogador
+    {
+         public int energia = 100;
+         public bool vivo = true;
+    }
+
+    class Aula28
+    {
+        static void Main()
+        {
+            Jogador j1 = new Jogador();
+            Jogador j2= new Jogador();
+            Jogador j3= new Jogador();
+
+            Console.WriteLine("Energia do jogador 1: {0}", j1.energia);
+            Console.WriteLine("Energia do jogador 2: {0}", j2.energia);
+            Console.WriteLine("Energia do jogador 3: {0}", j3.energia);
+
+
+        }
+
+    }
+}
